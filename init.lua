382868cbe5eb8a5a33c79a5d616abeb5d31afefa
mindcool24 <thezachm24@gmail.com>
2026-04-14 23:01:48 -0600

    Added version check for componentslib

--- init.lua
+++ init.lua
@@ -16,3 +16,15 @@ if tonumber(crafttools.VERSION) <= 20101 then
 		.. " is present."
 	)
 end
+
+if tonumber(COMPLIB.VERSION) <= 5 then
+	error(
+		"ComponentsLib-CraftTools Compatibility requires ComponentsLib version 0.0.5 or greater, but only version "
+		.. table.concat({
+			tonumber(COMPLIB.VERSION:sub(1,2)),
+			tonumber(COMPLIB.VERSION:sub(3,4)),
+			tonumber(COMPLIB.VERSION:sub(5,6))
+		}, '.')
+		.. " is present."
+	)
+end
